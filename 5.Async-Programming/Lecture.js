4f949afaa29b29f1319bf5dd75b74580cdf80d1d
panayotsky.dev <panayotsky.dev@gmail.com>
2022-11-01 23:39:20 +0200

    Update

--- 5.Async-Programming/Lecture.js
+++ 5.Async-Programming/Lecture.js
@@ -34,9 +34,69 @@
 
 // JavaScript e еднонишков език, което означава, че изпълнява само една команда в даден момент от времето
 // -.--.-.--->
+//Регистрираме EventHandler-i и тяхното управление.
 
 // Някои езици са многонишкови, което означава, че могат да изпълняват няколко команди едновременно
 // многонишков - multithreading
 // -.--.-.--->
 // --.-----.->
 // -.----.--->
+
+//*Asynchronous Programming in JS
+    //? Structured using callback functions
+    //? in current versions of JS there are:
+        // Callbacks
+        // Promises
+        // Async Functions
+    //?Not the same thing as concurrent or multi-threaded
+    //? JS code is generally single-threaded!
+
+//* Async programming - example
+ // callback е функцията която задаваме да бъде изпълнена по-късно / продължи изпълнението на нашият код
+    
+ 
+ 
+    // console.log("Hello.");
+    // setTimeout(function (){
+    //     console.log("Goodbye!");},2000)
+    // console.log("Hello again.");
+
+//*What is Promise ?
+
+    //A promise is an asynchronous action that may complete at some point and produce a value
+    
+    //?States
+        //Pending - operation still running (unfinished)
+        //Fulfilled - operation finished(the result is available)
+        //Failed - operation filed(an error is present)
+    //Promises use the Promise class
+    //  new Promise(executor);
+
+
+    //? Promise.then
+    // function logFetch(url){
+    //     return fetch(url)
+    //     .then(response => {
+    //         return response.text()
+    //     })
+    //     .then(text => {
+    //         console.log(text);
+    //     })
+    //     .catch(err => {
+    //         console.error(err);
+    //     })
+    // }
+
+    //? Async/Await
+    // async function logFetch(url){
+    //     try {
+    //         const response = 
+    //         await fetch(url);
+    //         console.log(
+    //             await response.text()
+    //         );
+    //     }
+    //     catch(err){
+    //         console.log(err);
+    //     }
+    // }
