5468973f515432059c8b6472414263d6ed023cfd
panayotsky.dev <panayotsky.dev@gmail.com>
2022-11-06 00:01:26 +0200

    Update app.js

--- 6.EXC-Async/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ 6.EXC-Async/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -18,13 +18,14 @@ function getInfo(){
 }
 
 
-    //Variant 2
+    // Variant 2
 // async function getInfo() {
 //     const stopId = document.getElementById('stopId').value;
 //     const stopName = document.getElementById('stopName');
 //     const busList = document.getElementById('buses');
 //     const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`
 //     busList.innerHTML = '';
+//     try {
 //     const response = await fetch(url);
 //     const data = await response.json()
 //     stopName.textContent = data.name;
@@ -33,4 +34,6 @@ function getInfo(){
 //         li.textContent = `Bus ${busNumber} arrives in ${timeToArrive} minutes`;
 //         busList.appendChild(li);
 //     })
-// }
+//     } catch (error) {
+//         stopName.textContent = 'Error';}
+//     }
