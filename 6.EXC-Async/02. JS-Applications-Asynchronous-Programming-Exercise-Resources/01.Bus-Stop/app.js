fb4c5d74d283ce2cb9970c621b3230c19093fb3d
panayotsky.dev <panayotsky.dev@gmail.com>
2022-11-05 21:33:14 +0200

    update

--- 6.EXC-Async/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ 6.EXC-Async/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,12 @@
 function getInfo() {
-    console.log("TODO...");
+    const stopId = document.getElementById('stopId').value;
+    const stopName = document.getElementById('stopName');
+    const buses = document.getElementById('buses');
+
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+    .then(response => response.json())
+    .then(data =>{
+        stopName.textContent = data.name;
+    })
+    .catch(error => console.log('error', error));
 }
