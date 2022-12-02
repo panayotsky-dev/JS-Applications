62e2ebbb37b7d88d7ed9a822a7b0db2d9bb36bc9
panayotsky.dev <panayotsky.dev@gmail.com>
2022-12-02 19:42:07 +0200

    update

---
+++ 19-2.ExamPrep/GamesPlay/src/api/games.js
@@ -0,0 +1,19 @@
+import * as api from './api.js'
+
+const endpoints = {
+    recent: '/data/games?sortBy=_createdOn%20desc&distinct=category',
+    games: '/data/games?sortBy=_createdOn%20desc',
+    create:'/data/games',
+};
+
+export async function getRecent(){
+    return api.get(endpoints.recent);
+}
+
+export async function getAll(){
+    return api.get(endpoints.games)
+}
+
+export async function create(data){
+    return api.post(endpoints.create,data)
+}
